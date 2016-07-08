b2202bf23c6e376a28fbdeee1b8b34fe4ef3f486
Maynard, Jacob <jacob_maynard@intuit.com>
2016-07-07 18:45:37 -0700

    Initial Commit
    Base functionality

---
+++ app/scripts/main.js
@@ -0,0 +1,56 @@
+// console.log('\'Allo \'Allo!');
+
+function fadeCard(elem, diff) {
+  // console.log('huzzah, diff =' + diff);
+  $(elem).css( "opacity", diff );
+}
+function fadeCardReset(elem) {
+  $(elem).css( "opacity", 1 );
+}
+function removeCard(elem) {
+  $(elem).slideUp("slow");
+}
+
+var draggable = Draggable.create(".draggable", {
+	type:"x",
+	lockAxis:true,
+	onClick:function() {
+		console.log("clicked");
+    //removeCard(this.target);
+	},
+  onDrag:function() {
+    // console.log("dragging");
+    var currentX = this.x;
+    var cardWidth = this.target.offsetWidth;
+    // console.log(currentX + "and the card is " + cardWidth + " wide");
+    if ( Math.abs(currentX) >= (cardWidth/4) ) {
+      var difference = 1-(Math.abs(currentX)/cardWidth);
+      fadeCard(this.target, difference);
+    }
+    else {
+      fadeCardReset(this.target);
+    }
+  },
+  onDragEnd:function(){
+    // console.log("drag ended");
+    var currentX = this.x;
+    var cardWidth = this.target.offsetWidth;
+    if ( Math.abs(currentX) >= (cardWidth/8*5) ) {
+      // console.log("get rid of this guy!")
+      if (currentX > 0) {
+        TweenLite.to(
+          this.target , 0.25 , { x:cardWidth+30 , y:0 , onComplete: removeCard, onCompleteParams:[this.target] }
+        );
+      }
+      else if (currentX < 0) {
+        TweenLite.to(
+          this.target , 0.25 , { x:-cardWidth-30 , y:0 , onComplete: removeCard, onCompleteParams:[this.target] }
+        );
+      }
+    }
+    else {
+      TweenLite.to( this.target , 1 , { x:0 , y:0 });
+      fadeCardReset(this.target);
+    }
+  }
+});
